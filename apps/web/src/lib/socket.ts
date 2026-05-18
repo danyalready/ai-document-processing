c33384859546cc851cf8081b473e434a1ee6b108
Doniyor <doniyor.otamurodov@gmail.com>
2026-05-18 14:31:26 +0500

    perf(docker): Optimize docker compose build process

--- apps/web/src/lib/socket.ts
+++ apps/web/src/lib/socket.ts
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
+console.log(process.env.NEXT_PUBLIC_API_URL);
+export const socket = io(process.env.NEXT_PUBLIC_API_URL);
