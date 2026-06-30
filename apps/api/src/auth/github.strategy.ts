1e160d27bee4bea938f3d30daeac8c63962dcbc0
Doniyor <doniyor.otamurodov@gmail.com>
2026-06-30 14:59:08 +0500

    feat(api): Fetch Github user via access-token

--- apps/api/src/auth/github.strategy.ts
+++ apps/api/src/auth/github.strategy.ts
@@ -10,20 +10,42 @@ export class GithubStrategy extends PassportStrategy(Strategy, "github") {
             clientID: configService.get("GITHUB_CLIENT_ID"),
             clientSecret: configService.get("GITHUB_CLIENT_SECRET"),
             callbackURL: configService.get("GITHUB_CALLBACK_URL"),
-            scope: ["email", "profile"],
         });
     }
 
     async validate(
-        _accessToken: string,
+        accessToken: string,
         _refreshToken: string,
         profile: Profile,
     ) {
-        console.log(profile);
+        const user = await this.getGithubUser(accessToken);
+
         return {
+            accessToken,
             githubId: profile.id,
             fullName: profile.displayName,
-            email: profile.emails?.[0]?.value,
+            email: user.email,
         };
+    }
+
+    async getGithubUser(accessToken: string): Promise<any | null> {
+        const response = await fetch("https://api.github.com/user", {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                Accept: "application/json",
+            },
+        });
+
+        console.log({ response });
+
+        if (!response.ok) {
+            throw new Error(
+                `Failed to fetch GitHub emails: ${response.status} ${response.statusText}`,
+            );
+        }
+
+        const user = await response.json();
+
+        return user ?? null;
     }
 }
